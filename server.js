69950dcc3ee999c86b32aae4df6587629d0716d2
paccyTech <irakozep9@gmail.com>
2025-06-14 12:26:27 +0200

    Added new domain to access Api

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ✅ Set your frontend origin
-const allowedOrigins = ['https://indatwa-cient.vercel.app'];
+const allowedOrigins = [
+  'https://indatwa-cient.vercel.app',
+  'https://indatwaevents.com'
+];
 
 app.use(cors({
   origin: function (origin, callback) {
